ee7997f5e225a0ae8ac553de80dbeddf7be38a1b
Michal Sara <michal.sara99@gmail.com>
2019-12-03 20:33:49 +0100

    Changes: Added few more examples to header files: gq_kronrod_t.h,gq_legendre_t.h,gq_lobatto_t.h

--- GaussQuadrature/gq_legendre_t.h
+++ GaussQuadrature/gq_legendre_t.h
@@ -50,8 +50,68 @@ void integrateExp5Legendre() {
 	std::cout << "18-point Gauss-Legendre integral value: " << integral18() << "\n";
 	std::cout << "20-point Gauss-Legendre integral value: " << integral20() << "\n";
 	std::cout << "\n\n";
+}
+
+void simpleIntegralsLegendre() {
+
+	using gq_legendre::GQLegendre;
+	using gq_legendre::Integrand;
+	using gq_utilities::Range;
+	using gq_legendre::NPoint;
+
+	auto inverseFun = [](double x) {return (1.0 / x); };
+	auto poly = [](double x) {return (1.0 + x * x); };
+
+	auto firstDomain = Range<double>{ 1.0,2.0 };
+	auto secondDomain = Range<double>{ 0.0,10.0 };
+
+	Integrand<double, double> firstIntegrand{ inverseFun,firstDomain };
+	Integrand<double, double> secondIntegrand{ poly,secondDomain };
+
+	auto integral_1 = GQLegendre<NPoint::_10, double>{ firstIntegrand };
+	auto integral_2 = GQLegendre<NPoint::_10, double>{ secondIntegrand };
+
+	std::cout << "10-point Gauss-Legendre integral value: " << integral_1() << "\n";
+	std::cout << "10-point Gauss-Legendre integral value: " << integral_2() << "\n";
+	std::cout << "===========================================\n";
+}
 
 
+
+void fresnelIntegralsLegendre() {
+	
+	using gq_legendre::GQLegendre;
+	using gq_utilities::PI;
+	using gq_utilities::Range;
+	using gq_utilities::NPoint;
+	using gq_interface::Integrand;
+
+	// Prepare integrands for fresnel integrals:
+	auto sinFun = [](double t) {return std::sin(0.5*PI*t*t); };
+	auto cosFun = [](double t) {return std::cos(0.5*PI*t*t); };
+
+	// Sine Fresnel integral:
+	// S(x) = int_{0}^{x}{sin(0.5*pi*t*t)dt}
+	auto S = [&](double x){
+		auto range = Range<double>{ 0.0,x };
+		auto integral = GQLegendre<NPoint::_20, double>{ Integrand<double, double>{ sinFun,range } };
+		return integral();
+	};
+
+	// Cosine Fresnel integral:
+	// C(x) = int_{0}^{x}{cos(0.5*pi*t*t)dt}
+	auto C = [&](double x) {
+		auto range = Range<double>{ 0.0,x };
+		auto integral = GQLegendre<NPoint::_20, double>{ Integrand<double, double>{ cosFun,range } };
+		return integral();
+	};
+
+	std::cout << "Sine Fresnel integral:\n";
+	std::cout << "True value S(6.0) = " << 0.44696 << "\n";
+	std::cout << "Approximated value S(6.0) = " << S(6.0) << "\n";
+	std::cout << "True value S(6.40) = " << 0.49646 << "\n";
+	std::cout << "Approximated value S(6.40) = " << S(6.40) << "\n";
+	std::cout << "========================================================\n";
 }
 
 
--- GaussQuadrature/gq_lobatto_t.h
+++ GaussQuadrature/gq_lobatto_t.h
@@ -55,6 +55,68 @@ void integrateExp5Lobatto() {
 }
 
 
+void simpleIntegralsLobatto() {
+
+	using gq_lobatto::GQLobatto;
+	using gq_legendre::Integrand;
+	using gq_utilities::Range;
+	using gq_legendre::NPoint;
+
+	auto inverseFun = [](double x) {return (1.0 / x); };
+	auto poly = [](double x) {return (1.0 + x * x); };
+
+	auto firstDomain = Range<double>{ 1.0,2.0 };
+	auto secondDomain = Range<double>{ 0.0,10.0 };
+
+	Integrand<double, double> firstIntegrand{ inverseFun,firstDomain };
+	Integrand<double, double> secondIntegrand{ poly,secondDomain };
+
+	auto integral_1 = GQLobatto<NPoint::_10, double>{ firstIntegrand };
+	auto integral_2 = GQLobatto<NPoint::_10, double>{ secondIntegrand };
+
+	std::cout << "10-point Gauss-Lobatto integral value: " << integral_1() << "\n";
+	std::cout << "10-point Gauss-Lobatto integral value: " << integral_2() << "\n";
+	std::cout << "===========================================\n";
+}
+
+
+void fresnelIntegralsLobatto() {
+
+	using gq_lobatto::GQLobatto;
+	using gq_utilities::PI;
+	using gq_utilities::Range;
+	using gq_utilities::NPoint;
+	using gq_interface::Integrand;
+
+	// Prepare integrands for fresnel integrals:
+	auto sinFun = [](double t) {return std::sin(0.5*PI*t*t); };
+	auto cosFun = [](double t) {return std::cos(0.5*PI*t*t); };
+
+	// Sine Fresnel integral:
+	// S(x) = int_{0}^{x}{sin(0.5*pi*t*t)dt}
+	auto S = [&](double x) {
+		auto range = Range<double>{ 0.0,x };
+		auto integral = GQLobatto<NPoint::_20, double>{ Integrand<double, double>{ sinFun,range } };
+		return integral();
+	};
+
+	// Cosine Fresnel integral:
+	// C(x) = int_{0}^{x}{cos(0.5*pi*t*t)dt}
+	auto C = [&](double x) {
+		auto range = Range<double>{ 0.0,x };
+		auto integral = GQLobatto<NPoint::_20, double>{ Integrand<double, double>{ cosFun,range } };
+		return integral();
+	};
+
+	std::cout << "Sine Fresnel integral:\n";
+	std::cout << "True value S(6.0) = " << 0.44696 << "\n";
+	std::cout << "Approximated value S(6.0) = " << S(6.0) << "\n";
+	std::cout << "True value S(6.40) = " << 0.49646 << "\n";
+	std::cout << "Approximated value S(6.40) = " << S(6.40) << "\n";
+	std::cout << "========================================================\n";
+}
+
+
 
 
 #endif //_GQ_LEGENDRE_T_H_
